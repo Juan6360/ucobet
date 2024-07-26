75fb9770c9c4ea975938bdeaee6fd1d772a7867b
Juan6360 <162759252+Juan6360@users.noreply.github.com>
2024-07-25 22:09:03 -0500

    first commit

--- src/com.juan/ucobet/app/AppUcoBet.java
+++ src/com.juan/ucobet/app/AppUcoBet.java
@@ -1,5 +1,7 @@
 package com.juan.ucobet.app;
 
+import com.juan.ucobet.dominio.Juego;
+
 public class AppUcoBet {
     public static void main(String[] args) {
 
---
+++ src/com.juan/ucobet/dominio/Juego.java
@@ -0,0 +1,8 @@
+package com.juan.ucobet.dominio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Juego {
+
+}
